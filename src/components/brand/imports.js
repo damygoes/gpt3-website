de39e01c5c09d15e6f45ff56c5e39379690ee416
Damilola Bada <badadamilola@gmx.de>
2022-06-09 19:06:05 +0200

    components, containers, imports, props and array mapping

---
+++ src/components/brand/imports.js
@@ -0,0 +1,9 @@
+// A helper file to import all images to the Brand component
+
+import google from "../../assets/google.png";
+import slack from "../../assets/slack.png";
+import atlassian from "../../assets/atlassian.png";
+import dropbox from "../../assets/dropbox.png";
+import shopify from "../../assets/shopify.png";
+
+export { google, slack, atlassian, dropbox, shopify };
